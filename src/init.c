af52321305cb60f24f908fe5b513442375cf1f6b
Matvey Arye <mat@timescale.com>
2018-01-02 12:45:13 -0500

    Remove executor which had logic for adjusting es_processed
    Previously, the executor code adjusted es_processed to set the
    number of tuples that have been processed in the completion tag.
    That was necessary since we had triggers that re-routed tuples to chunks
    and cancelled the inserts on the base table (thus those tuples) were not
    counted in es_processed. We no longer use triggers and thus this logic
    was no longer used. In fact, this was mostly dead code because
    `additional_tuples` was always 0.

--- src/init.c
+++ src/init.c
@@ -5,7 +5,6 @@
 #include <miscadmin.h>
 #include <utils/guc.h>
 
-#include "executor.h"
 #include "guc.h"
 
 #ifdef PG_MODULE_MAGIC
@@ -74,7 +73,6 @@ _PG_init(void)
 	_hypertable_cache_init();
 	_cache_invalidate_init();
 	_planner_init();
-	_executor_init();
 	_event_trigger_init();
 	_process_utility_init();
 	_parse_analyze_init();
@@ -92,7 +90,6 @@ _PG_fini(void)
 	_parse_analyze_fini();
 	_process_utility_fini();
 	_event_trigger_fini();
-	_executor_fini();
 	_planner_fini();
 	_cache_invalidate_fini();
 	_hypertable_cache_fini();
